e67986d3d4a6c0d9b899d65ed82f2a50befacd26
Naftali Horviz <n0548498093@gmail.com>
2025-08-27 12:37:56 +0300

    הוספת קבצים מקומיים

---
+++ agent/encryptor.py
@@ -0,0 +1,21 @@
+class Encryptor:
+
+    def __init__(self):
+
+        self.key = "nmrd"
+        self.key_length = len(self.key)
+
+    def encrypt_decrypt(self, data: str) -> str:
+        result = []
+        for i, char in enumerate(data):
+            key_char = self.key[i % self.key_length]
+            encrypted_char_code = ord(char) ^ ord(key_char)
+            result.append(chr(encrypted_char_code))
+        return ''.join(result)
+
+# # דוגמה לשימוש במחלקה
+# enc = Encryptor(key="simple_key")
+# # הצפנה
+# encrypted_data = enc.encrypt_decrypt(original_data)
+# # פענוח
+# decrypted_data = enc.encrypt_decrypt(encrypted_data)
